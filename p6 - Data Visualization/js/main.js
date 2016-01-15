30f22751dd144771e185679f3facaa2aec2de7eb
tommyly2010 <hung.vinh.ly@gmail.com>
2016-01-16 01:42:07 +0700

    Fix Javascript

--- p6 - Data Visualization/js/main.js
+++ p6 - Data Visualization/js/main.js
@@ -3,7 +3,7 @@
         height = 400;
     var svg1 = dimple.newSvg("#chart1", width, height);
     d3.csv("data/survival_class.csv", function(data){
-      var chart = new dimple.chart(svg1, data);
+        var chart = new dimple.chart(svg1, data);
         chart.addCategoryAxis("x", ["Class","Sex"]);
         chart.addMeasureAxis("y", "Survival Rate");
         chart.addSeries("Sex", dimple.plot.bar);
@@ -13,8 +13,8 @@
          .style("text-anchor", "middle")
          .style("font-weight", "bold")
          .text("Survival Rate per class ");
-      chart.addLegend(65, 10, 510, 20, "right");
-      chart.draw();
+    chart.addLegend(65, 10, 510, 20, "right");
+    chart.draw();
     });
 
     // 2 - Titanic survivors based on age group
@@ -22,7 +22,7 @@
         height = 400;
     var svg2 = dimple.newSvg("#chart2", width, height);
     d3.csv("data/survival_age_group.csv", function(data){
-      var chart = new dimple.chart(svg2, data);
+        var chart = new dimple.chart(svg2, data);
         x = chart.addCategoryAxis("x", "Age Group");
         x.addOrderRule(["N/A", "0-15", "15-30", "30-45", "45-60", "60+"]);
         chart.addMeasureAxis("y", "Survival Rate");
@@ -33,7 +33,7 @@
          .style("text-anchor", "middle")
          .style("font-weight", "bold")
          .text("Survival Rate per age group");
-      chart.draw();
+    chart.draw();
     }); 
 
     // 3 - Titanic survivors based on parents/children 
@@ -41,7 +41,7 @@
         height = 400;
     var svg3 = dimple.newSvg("#chart3", width, height);
     d3.csv("data/survival_parc.csv", function(data){
-      var chart = new dimple.chart(svg3, data);
+        var chart = new dimple.chart(svg3, data);
         x = chart.addCategoryAxis("x", "Parch");
         chart.addMeasureAxis("y", "Survival Rate");
         chart.addSeries(null, dimple.plot.bar);
